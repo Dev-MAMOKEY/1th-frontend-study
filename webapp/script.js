eaeb5449c62d41212e3ba26d7f7960a9dfe40b67
ohsanghy09 <ohsanghy09@naver.com>
2026-05-08 13:05:11 +0900

    feat : 장바구니 추가, 삭제, 수량 조정 작성

--- webapp/script.js
+++ webapp/script.js
@@ -13,9 +13,9 @@ const totalPrice = document.querySelector("#total-price");
 
 // 상품 데이터
 const products = [
-  { id: 1, name: "노트북", price: 1200000 },
-  { id: 2, name: "마우스", price: 30000 },
-  { id: 3, name: "키보드", price: 80000 }
+    { id: 1, name: "노트북", price: 1200000 },
+    { id: 2, name: "마우스", price: 30000 },
+    { id: 3, name: "키보드", price: 80000 }
 ];
 
 
@@ -29,48 +29,170 @@ function showProducts() {
 
     // 생산품 리스트 비우기 -> 안 비우면 중복으로 출력
     productList.innerHTML = "";
-    console.log("리스트 비웠음");
 
     // 생산품 데이터 하나씩 생산품 리스트에 넣음
-    products.forEach( function (product) {
-
-        console.log(product);
+    products.forEach(function (product) {
 
         // div요소 생성 후 productItem으로 할당
         const productItem = document.createElement("div");
 
         // div 요소, 즉 productItem 하나 당 클래스 적용
         productItem.classList.add("product-item");
-        
-        // div(productItem) 요소 삽입
+
+        // div(productItem)에 요소 삽입
         productItem.innerHTML = `
             <span>${product.name}</span>
             <strong>${product.price}</strong>
-            <button class="add-btn" data-id=${product.id}>추가</button>
+            <button class="add-btn" data-id="${product.id}">추가</button>
         `;
 
         // 만든 div(productItem)을 생산품 리스트에 넣기
         productList.appendChild(productItem);
 
-    })   
+    })
 }
 
-productList.addEventListener("click", function (event){
+// 장바구니에 추가 이벤트 리스너 적용
+productList.addEventListener("click", function (event) {
 
     // 만약 클릭한 요소의 class에 "add-btn"이 있다면
-    if (event.target.classList.contains("add-btn")){
+    if (event.target.classList.contains("add-btn")) {
 
         //클릭 요소 data- 형태의 요소 출력
         const productId = Number(event.target.dataset.id);
 
         // 클릭한 요소가 실제 상품데이터와 일치하는지 확인
-        const selectedProduct = products.find(function (product){ // find - true면 해당 객체 반환
+        const selectedProduct = products.find(function (product) { // find - true면 해당 객체 반환
             return product.id === productId; // true, false 출력
         });
 
-        // cart 배열에 삽입
-        cart.push(selectedProduct);
-        
+        // 클릭한 요소가 장바구니에 있는지 확인
+        const selectedCart = cart.find(function (item) { // find - true면 해당 객체 반환
+            return item.id === productId; // true, false 출력
+        });
+
+        // 이미 장바구니에 있으면 수량만 증가
+        if (selectedCart) {
+            selectedCart.quantity += 1;
+        }
+
+        // 장바구니에 없으면 새로 추가 (quantity: 1 속성 추가)
+        else {
+            cart.push({
+                id: selectedProduct.id,
+                name: selectedProduct.name,
+                price: selectedProduct.price,
+                quantity: 1
+            });
+        }
+
+        // 장바구니 데이터 갱신
+        showCart();
+
+    }
+})
+
+
+// 화면에 장바구니 데이터 출력 (DOM : cartList)
+function showCart() {
+
+    // 생산품 리스트 비우기 -> 안 비우면 중복으로 출력
+    cartList.innerHTML = "";
+
+    // 생산품 데이터 하나씩 생산품 리스트에 넣음 (배열 : cart)
+    cart.forEach(function (item) {
+
+        // div요소 생성 후 cartItem으로 할당
+        const cartItem = document.createElement("div");
+
+        // div 요소, 즉 cartItem 하나 당 클래스 적용
+        cartItem.classList.add("cart-item");
+
+        // div(carttItem)에 요소 삽입 
+        cartItem.innerHTML = `
+            <span>${item.name}</span>
+            <strong>${item.price}</strong>
+            <span>수량 : ${item.quantity}</span>
+            <button class="minus-btn" data-id="${item.id}">-1</button>
+            <button class="delete-btn" data-id="${item.id}">목록 삭제</button>
+        `;
+
+        // 만든 div(productItem)을 생산품 리스트에 넣기
+        cartList.appendChild(cartItem);
+
+    })
+}
+
+
+// -1 클릭 시 동작
+cartList.addEventListener("click", function (event) {
+
+    // 만약 클릭한 요소의 class에 "minus-btn"이 있다면
+    if (event.target.classList.contains("minus-btn")) {
+
+        //클릭 요소 data- 형태의 요소 출력
+        const cartId = Number(event.target.dataset.id);
+
+        // cart 배열에서 클릭한 상품 확인 (find 함수는 객체 반환)
+        const selectedCart = cart.find(function (item) {
+            return item.id === cartId;
+        });
+
+        // 찾은 상품의 수량을 1 감소
+        selectedCart.quantity -= 1;
+
+        // 만약 수량이 1미만이 되면 cart 배열에서 제거
+        if (selectedCart.quantity < 1) {
+
+            // 삭제 전 경고창
+            const result = confirm(`${selectedCart.name}의 모든 수량을 장바구니에서 삭제할까요?`);
+
+            // 사용자가 취소를 누르면 해당 시점에서 코드 종료
+            if (result === false) {
+                return;
+            }
+
+            // cart 배열에서 cartId와 다른 id를 가진 상품만 남김
+            cart = cart.filter(function (item) { // filter는 true만 골라서 재배열
+                return item.id !== cartId; // 같지 않다면 true, 같으면 false
+            });
+        }
+
+        // 장바구니 화면 다시 출력
+        showCart();
+    }
+})
+
+
+// 목록 삭제 클릭 시 동작
+cartList.addEventListener("click", function (event) {
+
+    // 만약 클릭한 요소의 class에 "delete-btn"이 있다면
+    if (event.target.classList.contains("delete-btn")) {
+
+        //클릭 요소 data- 형태의 요소 출력
+        const cartId = Number(event.target.dataset.id);
+
+        // cart 배열에서 클릭한 상품 확인 (find 함수는 객체 반환)
+        const selectedCart = cart.find(function (item) {
+            return item.id === cartId;
+        });
+
+        // 삭제 전 경고창
+        const result = confirm(`${selectedCart.name}의 모든 수량을 장바구니에서 삭제할까요?`);
+
+        // 사용자가 취소를 누르면 해당 시점에서 코드 종료
+        if (result === false) {
+            return;
+        }
+
+        // cart 배열에서 cartId와 다른 id를 가진 상품만 남김
+        cart = cart.filter(function (item) { // filter는 true만 골라서 재배열
+            return item.id !== cartId; // 같지 않다면 true, 같으면 false
+        });
+
+        // 장바구니 화면 다시 출력
+        showCart();
     }
 })
 
