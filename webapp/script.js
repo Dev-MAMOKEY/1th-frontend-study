bae1e88ce8051630371486953e4b0649839b2c79
ohsanghy09 <ohsanghy09@naver.com>
2026-05-08 11:31:31 +0900

    feat : 기본 인터페이스 적용(index.html, style.css), 화면에 상품 데이터 출력 함수 작성(script.js)

---
+++ webapp/script.js
@@ -0,0 +1,71 @@
+// DOM 선택 querySelector
+
+// 생산품 리스트
+const productList = document.querySelector("#product-list");
+
+// 카트 리스트
+const cartList = document.querySelector("#cart-list");
+
+// 총 합계
+const totalPrice = document.querySelector("#total-price");
+
+
+
+// 상품 데이터
+const products = [
+  { id: 1, name: "노트북", price: 1200000 },
+  { id: 2, name: "마우스", price: 30000 },
+  { id: 3, name: "키보드", price: 80000 }
+];
+
+
+
+// 장바구니 데이터
+let cart = [];
+
+
+// 화면에 상품 데이터 출력 (DOM : productList)
+function showProducts() {
+
+    // 생산품 리스트 비우기 -> 안 비우면 중복으로 출력
+    productList.innerHTML = "";
+    console.log("리스트 비웠음");
+
+    // 생산품 데이터 하나씩 생산품 리스트에 넣음
+    products.forEach( function (product) {
+
+        console.log(product);
+
+        // div요소 생성 후 productItem으로 할당
+        const productItem = document.createElement("div");
+
+        // div 요소, 즉 productItem 하나 당 클래스 적용
+        productItem.classList.add("product-item");
+        
+        // div(productItem) 요소 삽입
+        productItem.innerHTML = `
+            <span>${product.name}</span>
+            <strong>${product.price}</strong>
+            <button class="add-btn" data-id=${product.id}>추가</button>
+        `;
+
+        // 만든 div(productItem)을 생산품 리스트에 넣기
+        productList.appendChild(productItem);
+
+    })   
+}
+
+productList.addEventListener("click", function (event){
+
+    // 만약 클릭한 요소의 class에 "add-btn"이 있다면
+    if (event.target.classList.contains("add-btn")){
+
+        //data- 형태의 요소 출력
+        console.log(event.target.dataset.id);
+    }
+})
+
+
+
+// 화면 갱신
+showProducts();
